f7e79f8e8ba3311c4101385ea62174f4b940c893
the-kulo <1079093921@qq.com>
2025-06-12 23:26:01 +0800

    feat(登录): 添加登录页面组件替换占位内容

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,12 @@
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
+import Login from './pages/login';
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
       <div className="app">
-        {/* 这里后续会添加路由 */}
-        <h1>Hyper-CMDB</h1>
-        <p>登录页面开发中...</p>
+        <Login />
       </div>
     </ConfigProvider>
   );
