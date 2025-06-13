729bf2cdc43a6dc55765e6d3e0fa60db0368129a
the-kulo <1079093921@qq.com>
2025-06-14 00:01:06 +0800

    build(ui): 配置vite构建输出目录和基础路径

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Hyper-CMDB/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
